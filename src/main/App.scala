64012811c43e33d4f1733b985f898ff43f44955f
Ziółkowski Krzysztof <ziolkowski15@gmail.com>
2019-02-04 19:00:22 +0100

    Added isCheck, arrange, setPieces(doesn't work) methods.

--- src/main/App.scala
+++ src/main/App.scala
@@ -1,5 +1,28 @@
 package main
 
-class App1 (width: Int, height: Int, pieces: List[(Int, Int) => Piece]) {
+class App1 (width: Int, height: Int, pieces: List[(Int, Int) => Piece]){
+
+  def isCheck(p1: Piece, p2: Piece): Boolean = p1.isAttacking(p2) || p2.isAttacking(p1)
+
+  def arrange(p: (Int, Int) => Piece, x: Int, y: Int): Piece = p match {
+    case Bishop => Bishop(x, y)
+    case King => King(x, y)
+    case Knight => Knight(x, y)
+    case Rook => Rook(x, y)
+    case Queen => Queen(x, y)
+    case _ => throw new IllegalArgumentException("Incorrect name of the figure.")
+  }
+
+  def setPieces(pieces: List[(Int, Int) => Piece]) = pieces match {
+    case Nil => Set()
+    case head :: tail => for {
+      pieces <- setPieces(tail)
+      w <- 1 to width
+      h <- 1 to height
+      piece = arrange(head, w, h)
+      if (!isCheck(piece, _))
+    } yield (piece :: pieces)
+  }
+  //TODO: MAKE IT WORK!
 
 }
--- src/test/AppTest.scala
+++ src/test/AppTest.scala
@@ -7,11 +7,31 @@ object AppTest extends App {
   val test = new App1(4, 4, List(King, King))
 
   val king = King(2, 2)
+  val queen = Queen(2, 2)
+  val bishop = Bishop(2, 2)
 
-  val safeTest1 = for {
+  val checkTest1 = for {
     w <- 1 to 4
     h <- 1 to 4
-  } yield println("King on (2,2), Queen on (" + w + "," + h + "). Is check: " + test.isSafe(king, Queen(w, h)))
+  } yield println("King on (2,2), Queen on (" + w + "," + h + "). Is check: " + test.isCheck(king, Queen(w, h)))
 
-  safeTest1
+  checkTest1
+
+  val checkTest2 = for {
+    w <- 1 to 5
+    h <- 1 to 5
+  } yield println("Queen on (2,2), Rook on (" + w + "," + h + "). Is check: " + test.isCheck(queen, Rook(w, h)))
+
+  checkTest2
+
+  val checkTest3 = for {
+    w <- 1 to 5
+    h <- 1 to 5
+  } yield println("Bishop on (2,2), Knight on (" + w + "," + h + "). Is check: " + test.isCheck(bishop, Knight(w, h)))
+
+  checkTest3
+
+  println()
+
+
 }
